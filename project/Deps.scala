9a2843c15cb249658d21f723ac0170cb1a6a8426
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-08-13 11:29:20 +0200

    Switch to the latest Ammonite
    This transitively bumps the version of coursier too

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Deps {
 
-  def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % "1.1.2-17-7d454d7").cross(CrossVersion.full)
+  def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % "1.1.2-25-5ef5ee1").cross(CrossVersion.full)
   def jettyServer = "org.eclipse.jetty" % "jetty-server" % "8.1.14.v20131031"
   def sparkSql = "org.apache.spark" %% "spark-sql" % "2.0.2" // no need to bump that version much, to ensure we don't rely on too new stuff
   def utest = "com.lihaoyi" %% "utest" % "0.6.4"
