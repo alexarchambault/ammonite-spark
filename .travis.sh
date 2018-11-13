2a463776e3490e1b0c3906ffdc7a7c3d073bd659
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-11-13 16:54:45 +0100

    Actually add CI job

--- .travis.sh
+++ .travis.sh
@@ -8,6 +8,8 @@ case "${MASTER:-"local"}" in
     ./sbt-with-standalone-cluster.sh ++$TRAVIS_SCALA_VERSION'!' publishLocal standalone-tests/test ;;
   yarn)
     ./sbt-in-docker-with-yarn-cluster.sh -batch ++$TRAVIS_SCALA_VERSION'!' publishLocal yarn-tests/test ;;
+  yarn-distrib)
+    ./with-spark-home.sh ./sbt-in-docker-with-yarn-cluster.sh -batch ++$TRAVIS_SCALA_VERSION'!' publishLocal yarn-spark-distrib-tests/test ;;
   *)
     echo "Unrecognized master type $MASTER"
     exit 1
