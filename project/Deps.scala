e1bc1bd44abefe51cd2554bf98ccc2e12da91029
Scala steward <me@scala-steward.org>
2018-12-18 19:59:54 +0100

    Update jetty-server to 8.1.22.v20160922

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ import sbt.Keys._
 object Deps {
 
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % "1.5.0").cross(CrossVersion.full)
-  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "8.1.14.v20131031"
+  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "8.1.22.v20160922"
   def utest = "com.lihaoyi" %% "utest" % "0.6.4"
 
   def sparkSql20 = "org.apache.spark" %% "spark-sql" % "2.0.2" // no need to bump that version much, to ensure we don't rely on too new stuff
