faa7c77821c716fc2cb5b565f6c68339e9883328
Scala steward <me@scala-steward.org>
2019-04-01 13:49:36 +0200

    Update spark-sql to 2.4.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.15.v20190215"
   def utest = "com.lihaoyi" %% "utest" % "0.6.6"
 
-  def sparkSql20 = "org.apache.spark" %% "spark-sql" % "2.0.2" // no need to bump that version much, to ensure we don't rely on too new stuff
+  def sparkSql20 = "org.apache.spark" %% "spark-sql" % "2.4.1" // no need to bump that version much, to ensure we don't rely on too new stuff
   def sparkSql24 = "org.apache.spark" %% "spark-sql" % "2.4.0" // that version's required for scala 2.12
   def sparkSql = setting {
     if (Settings.isAtLeast212.value)
