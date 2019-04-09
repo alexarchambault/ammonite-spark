ccc6c92c9d10a6bff421005cb68d14284e2fb443
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-04-09 09:51:43 +0200

    Update utest to 0.6.7 (#35)

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % "1.6.5").cross(CrossVersion.full)
   def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.15.v20190215"
-  def utest = "com.lihaoyi" %% "utest" % "0.6.6"
+  def utest = "com.lihaoyi" %% "utest" % "0.6.7"
 
   def sparkSql20 = "org.apache.spark" %% "spark-sql" % "2.0.2" // no need to bump that version much, to ensure we don't rely on too new stuff
   def sparkSql24 = "org.apache.spark" %% "spark-sql" % "2.4.0" // that version's required for scala 2.12
