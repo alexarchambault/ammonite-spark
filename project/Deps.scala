c0c059930fc77ecf06be5905677a789b5bb489a3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-08-14 20:28:06 +0200

    Update jetty-server to 9.4.20.v20190813 (#63)

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
-  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.19.v20190610"
+  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.20.v20190813"
   def utest = "com.lihaoyi" %% "utest" % "0.7.1"
 
   def sparkSql = "org.apache.spark" %% "spark-sql" % "2.4.0"
