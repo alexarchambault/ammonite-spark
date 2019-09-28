027ac2cdd3801c6f22b3334d1653cb2a6746d0ec
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2019-09-28 15:52:40 +0200

    Switch to Ammonite 1.7.3-3-b95f921 (#79)
    Seems Ammonite 1.7.2 was borked

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import sbt.Keys._
 
 object Deps {
 
-  private def ammoniteVersion = "1.7.1"
+  private def ammoniteVersion = "1.7.3-3-b95f921"
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
