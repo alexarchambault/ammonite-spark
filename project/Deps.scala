4e7fec4c9c1402fb66b19da8afd6f52430ac277c
Scala Steward <me@scala-steward.org>
2019-12-27 17:18:13 +0100

    Update ammonite-repl, ammonite-repl-api to 2.0.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import sbt.Keys._
 
 object Deps {
 
-  private def ammoniteVersion = "1.9.9"
+  private def ammoniteVersion = "2.0.1"
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
