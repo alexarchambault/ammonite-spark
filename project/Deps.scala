fde59ad9835c8888ea27cf74fcfc7a5fdd7a32f8
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2020-12-24 02:48:39 +0100

    Update Ammonite to 2.3.8-4-88785969

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import sbt.Keys._
 
 object Deps {
 
-  private def ammoniteVersion = "2.3.8"
+  private def ammoniteVersion = "2.3.8-4-88785969"
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
