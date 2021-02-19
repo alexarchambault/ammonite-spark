ff67c78ccb13ede084879bf331e4de16f712599b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-19 17:40:16 +0100

    Update utest to 0.7.7 (#170)

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
   def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.36.v20210114"
-  def utest = "com.lihaoyi" %% "utest" % "0.7.5"
+  def utest = "com.lihaoyi" %% "utest" % "0.7.7"
 
   def sparkSql = "org.apache.spark" %% "spark-sql" % "2.4.0"
   def sparkSql3 = "org.apache.spark" %% "spark-sql" % "3.0.0"
