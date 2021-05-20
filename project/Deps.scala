cc96c6504e3776ca3905c2867aba82f132b24f29
Scala Steward <me@scala-steward.org>
2021-05-21 00:06:30 +0200

    Update ammonite-compiler, ammonite-repl, ... to 2.3.8-124-2da846d2

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import sbt.Keys._
 
 object Deps {
 
-  private def ammoniteVersion = "2.3.8-122-9be39deb"
+  private def ammoniteVersion = "2.3.8-124-2da846d2"
   def ammoniteCompiler = ("com.lihaoyi" % "ammonite-compiler" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
