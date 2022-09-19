c61763bb606750b86524f76f2f2d6a3d22df74a4
Scala Steward <scala_steward@virtuslab.com>
2022-09-19 12:29:56 +0000

    Update jetty-server to 11.0.12

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,7 +15,7 @@ object Deps {
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
 
-  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.49.v20220914"
+  def jettyServer = "org.eclipse.jetty" % "jetty-server" % "11.0.12"
   def utest = "com.lihaoyi" %% "utest" % "0.8.1"
 
   def sparkSql = setting {
