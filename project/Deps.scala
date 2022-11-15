8a22d98e16856f8c1d62356c97f187b150ef813b
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2022-11-15 16:06:52 +0100

    Update Ammonite to 2.5.5-16-8d71f4bb

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
     def scala213 = "2.13.10"
   }
 
-  private def ammoniteVersion = "2.5.5-15-277624cf"
+  private def ammoniteVersion = "2.5.5-16-8d71f4bb"
   def ammoniteCompiler = ("com.lihaoyi" % "ammonite-compiler" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
