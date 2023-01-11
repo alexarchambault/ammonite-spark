61847da9b767be2ce672b5655cefdd3ebd45b494
Scala Steward <scala_steward@virtuslab.com>
2023-01-11 13:07:30 +0000

    Update ammonite-compiler, ammonite-repl, ... to 2.5.6

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
     def scala213 = "2.13.10"
   }
 
-  private def ammoniteVersion = "2.5.5-17-df243e14"
+  private def ammoniteVersion = "2.5.6"
   def ammoniteCompiler = ("com.lihaoyi" % "ammonite-compiler" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteReplApi = ("com.lihaoyi" % "ammonite-repl-api" % ammoniteVersion).cross(CrossVersion.full)
   def ammoniteRepl = ("com.lihaoyi" % "ammonite-repl" % ammoniteVersion).cross(CrossVersion.full)
